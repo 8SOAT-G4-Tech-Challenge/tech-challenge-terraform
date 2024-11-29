4f88b58605d39ea531ca4c5eb56d9dacf5414ca4
Winderson Santos <winderson.wjbs@gmail.com>
2024-11-29 01:25:33 -0300

    fix: altera porta utilizad para expor a aplicacao

--- eks/load-balancer.tf
+++ eks/load-balancer.tf
@@ -14,14 +14,14 @@ resource "aws_lb" "tc_load_balancer" {
 
 resource "aws_lb_target_group" "tc_lb_target_group" {
   name        = "lb-target-group-${var.tech_challenge_project_name}"
-  port        = 30007
+  port        = 31333
   protocol    = "HTTP"
   target_type = "instance"
   vpc_id      = data.aws_vpc.vpc.id
 
   health_check {
     path    = "/"
-    port    = 30007
+    port    = 31333
     matcher = "200"
   }
 }
@@ -39,6 +39,6 @@ resource "aws_lb_listener" "tc_lb_listener" {
 resource "aws_lb_target_group_attachment" "tc_balancer_attach" {
     target_group_arn = aws_lb_target_group.tc_lb_target_group.arn
     target_id = data.aws_instance.ec2.id
-    port = 30007
+    port = 31333
     depends_on = [ aws_eks_node_group.tc_node_group ]
 }
