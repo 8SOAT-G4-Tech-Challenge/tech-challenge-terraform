a4f94eb04a8386285392c166ed4b749235b466e0
Lucas Accurcio <lucas.accurcio@gmail.com>
2024-12-10 19:23:55 -0300

    styles: clean code

--- backend.tf
+++ backend.tf
@@ -1,7 +1,8 @@
 terraform {
   /* backend "s3" {
-    bucket = "tech-challenge-terraform"
+    bucket = "tech-challenge-bucket-state-tf"
     key    = "eks/terraform.tfstate"
     region = "us-east-1"
+		encrypt = true
   } */
 }
