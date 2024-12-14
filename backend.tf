a8aba81e13111a6155a7cc2613f546af5208f1e7
Winderson Santos <winderson.wjbs@gmail.com>
2024-12-14 19:04:11 -0300

    fix: update pipeline

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "s3" {
-    bucket = "tech-challenge-terraform-state"
-    key    = "infra/terraform.tfstate"
-    region = "us-east-1"
-		encrypt = true
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "tech-challenge-terraform-state"
+#     key    = "infra/terraform.tfstate"
+#     region = "us-east-1"
+# 		encrypt = true
+#   }
+# }
