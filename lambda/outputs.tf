3af460bfb6be749a03c5f4a365260bdab117d1cf
Winderson Santos <winderson.wjbs@gmail.com>
2024-12-14 16:24:53 -0300

    fix: remove diretorios relacionados ao recurso S3 e lambda

--- lambda/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "authenticate_admin_lambda_name" {
-	value = aws_lambda_function.authenticate_admin.function_name
-}
--- lambda/variables.tf
+++
@@ -1,8 +0,0 @@
-variable project_name {
-	description = "The name of the project"
-	type        = string
-}
-variable aws_account_id {
-	description = "The AWS Account ID"
-	type        = string
-}
